4b6e0cb335cf26e9dde3953e26ed7d7930fe55bd
Mario <mario.zweidick2@gmail.com>
2021-11-22 18:44:37 +0100

    Added getter and setter methods for class BMICalculator.java Furthermore added constructor for same class

--- src/main/java/BMICalculator.java
+++ src/main/java/BMICalculator.java
@@ -1,5 +1,56 @@
 public class BMICalculator
 {
     //Mario Zweidick
-    //
+    //Weight: 80kg
+    //Age: 26
+
+    String firstname;
+    String lastname;
+    int bodyHeight;
+    double bodyWeight;
+    char gender;
+
+    public BMICalculator(String firstname, String lastname, int bodyHeight, double bodyWeight, char gender)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.bodyHeight = bodyHeight;
+        this.bodyWeight = bodyWeight;
+        this.gender = gender;
+    }
+
+    public void setBodyHeight(int bodyHeight)
+    {
+        this.bodyHeight = bodyHeight;
+    }
+
+    public void setBodyWeight(double bodyWeight)
+    {
+        this.bodyWeight = bodyWeight;
+    }
+
+    public String getFirstname()
+    {
+        return firstname;
+    }
+
+    public String getLastname()
+    {
+        return lastname;
+    }
+
+    public int getBodyHeight()
+    {
+        return bodyHeight;
+    }
+
+    public double getBodyWeight()
+    {
+        return bodyWeight;
+    }
+
+    public char getGender()
+    {
+        return gender;
+    }
 }
